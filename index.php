e0c3163725adb7db74dd99a9b2abeee5215ddc11
logikal16 <logikal16@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-01 02:16:24 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/stripe-donations/trunk@632527 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,184 @@
+<?php
+/*
+Plugin Name: Stripe Donations
+Plugin URI: https://uproot.us/
+Description: Accept donations on your site using Stripe.
+Version: 1.0.0
+Author: Matt Gibbs
+Author URI: https://uproot.us/
+License: GPL2
+*/
+
+$sd = new StripeDonations();
+
+class StripeDonations
+{
+    public $dir;
+    public $url;
+    public $options;
+
+    /*============================================================
+        __construct
+    ============================================================*/
+
+    function __construct() {
+        $this->dir = (string) dirname(__FILE__);
+        $this->url = plugins_url('stripe-donations');
+        $this->options = get_option('ssd_options');
+
+        // assign defaults
+        if (!isset($this->options['secret_key'])) {
+            $this->options['secret_key'] = '';
+        }
+        if (!isset($this->options['publishable_key'])) {
+            $this->options['publishable_key'] = '';
+        }
+
+        // include the stripe library
+        require_once($this->dir . '/lib/Stripe.php');
+
+        // get the stripe keys
+        $stripe = array(
+            'secret_key' => $this->options['secret_key'],
+            'publishable_key' => $this->options['publishable_key'],
+        );
+
+        Stripe::setApiKey($stripe['secret_key']);
+
+        add_action('init', array($this, 'init'));
+        add_action('wp_ajax_ssd_save_options', array($this, 'save_options'));
+        add_action('wp_ajax_ssd_submit_payment', array($this, 'submit_payment'));
+        add_action('wp_ajax_nopriv_ssd_submit_payment', array($this, 'submit_payment'));
+        add_action('admin_menu', array($this, 'admin_menu'));
+        add_shortcode('ssd', array($this, 'shortcode'));
+    }
+
+    /*============================================================
+        init
+    ============================================================*/
+
+    function init() {
+        wp_enqueue_script('jquery');
+    }
+
+    /*============================================================
+        admin_menu
+    ============================================================*/
+
+    function admin_menu() {
+        add_options_page(
+            'Stripe Donations',
+            'Stripe Donations',
+            'manage_options',
+            'stipe-donations',
+            array($this, 'options_page')
+        );
+    }
+
+    /*============================================================
+        options_page
+    ============================================================*/
+
+    function options_page() {
+    ?>
+    <script>
+    (function($) {
+        $(function() {
+            $('#submit').click(function() {
+                $('#message').hide();
+                var data = {
+                    'action': 'ssd_save_options',
+                    'secret_key': $('#ssd_secret_key').val(),
+                    'publishable_key': $('#ssd_publishable_key').val()
+                };
+                $.post(ajaxurl, data, function(response) {
+                    $('#message p').html(response);
+                    $('#message').show();
+                });
+            });
+        });
+    })(jQuery);
+    </script>
+
+    <div class="wrap">
+        <h2>Stripe Donations</h2>
+        <div id="message" class="updated hidden"><p></p></div>
+        <p>To find your keys, log into your <a href="https://stripe.com/" target="_blank">Stripe.com</a> account and look in the "Account Settings" menu.</p>
+        <table class="form-table">
+            <tbody>
+                <tr>
+                    <th scope="row">Secret Key</th>
+                    <td><input type="text" id="ssd_secret_key" class="regular-text" value="<?php echo esc_attr($this->options['secret_key']); ?>" />
+                </tr>
+                <tr>
+                    <th scope="row">Publishable Key</th>
+                    <td><input type="text" id="ssd_publishable_key" class="regular-text" value="<?php echo esc_attr($this->options['publishable_key']); ?>" />
+                </tr>
+            </tbody>
+        </table>
+        <p class="submit">
+            <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+        </p>
+    </div>
+    <?php
+    }
+
+    /*============================================================
+        shortcode
+    ============================================================*/
+
+    function shortcode($atts) {
+        $amount = isset($atts['amount']) ? $atts['amount'] : '1000';
+    ?>
+    <script>
+    (function($) {
+        $(function() {
+            $('.stripe-button').bind('token', function(e, token) {
+                var data = {
+                    'action': 'ssd_submit_payment',
+                    'token': token.id,
+                    'amount': $(this).attr('data-amount')
+                };
+                $.post('<?php echo admin_url('admin-ajax.php'); ?>', data, function(response) {
+                    console.log(response);
+                });
+            });
+        });
+    })(jQuery);
+    </script>
+    <script src="https://button.stripe.com/v1/button.js" class="stripe-button"
+        data-key="<?php echo $this->options['publishable_key']; ?>"
+        data-amount="<?php echo $amount; ?>"
+        data-label="Donate"></script>
+    <?php
+    }
+
+    /*============================================================
+        save_options
+    ============================================================*/
+
+    function save_options() {
+        $data = array(
+            'secret_key' => $_POST['secret_key'],
+            'publishable_key' => $_POST['publishable_key'],
+        );
+        update_option('ssd_options', $data);
+        echo 'Settings saved.';
+        exit;
+    }
+
+    /*============================================================
+        submit_payment
+    ============================================================*/
+
+    function submit_payment() {
+        $token = isset($_POST['token']) ? $_POST['token'] : '';
+        $amount = isset($_POST['amount']) ? $_POST['amount'] : 0;
+        $charge = Stripe_Charge::create(array(
+            'card' => $token,
+            'amount' => $amount,
+            'currency' => 'usd',
+        ));
+        exit;
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== Stripe Donation ===
+Contributors: logikal16
+Donate link: https://uproot.us/donate/
+Tags: donation, donate, stripe, payment
+Requires at least: 3.4
+Tested up to: 3.5
+Stable tag: trunk
+License: GPL2
+
+Accept donations on your site using the Stripe Button.
+
+== Description ==
+
+Accept donations on your site using the Stripe Button.
+
+== Installation ==
+
+1. Download and activate the plugin.
+2. Browse to `Settings > Stripe Donations` and enter your Stripe keys.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
